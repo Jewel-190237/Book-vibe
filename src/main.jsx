8745c73f73c676fd9c163f8bf48150cd2bb4ddd8
Jewel-190237 <190237@ku.ac.bd>
2024-03-25 22:12:24 +0600

    header and banner added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,58 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Components/Root/Root.jsx';
+import ErrorPage from './Components/ErrorPage/ErrorPage.jsx';
+import Home from './Components/Home/Home.jsx';
+import Navbar from './Components/Navbar/Navbar.jsx';
+import Banner from './Components/Banner/Banner.jsx';
+import PagesToRead from './Components/PagesToRead/PagesToRead.jsx';
+import ListedBooks from './Components/ListedBooks/ListedBooks.jsx';
+import Books from './Components/Books/Books.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/navbar',
+        element: <Navbar></Navbar>
+      },
+      {
+        path: '/banner',
+        element: <Banner></Banner>
+      },
+      {
+        path: '/listedBooks',
+        element: <ListedBooks></ListedBooks>
+      },
+      {
+        path: '/pagesToRead',
+        element: <PagesToRead></PagesToRead>
+      },
+      {
+        path:'/books',
+        element: <Books></Books>,
+        loader: () => fetch('../public/Books.json')
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
