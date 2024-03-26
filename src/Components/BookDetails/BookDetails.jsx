c7664fa7a412201fdc39c546c219d34a7b59b42c
Jewel-190237 <190237@ku.ac.bd>
2024-03-26 19:20:19 +0600

    listed book updated

--- src/Components/BookDetails/BookDetails.jsx
+++ src/Components/BookDetails/BookDetails.jsx
@@ -4,14 +4,25 @@
 import { useLoaderData, useParams } from "react-router-dom";
 
 const BookDetails = () => {
-    
+
     const books = useLoaderData();
     const { bookId } = useParams()
     const idInt = parseInt(bookId)
     const book = books.find(book => book.bookId == idInt)
     return (
         <div>
-            <h2>Jewel: {book.bookId}</h2>
+            <div>
+            <div className="hero min-h-screen bg-[#1313130D]">
+                <div className="hero-content flex-col lg:flex-row gap-8">
+                    <img className='w-full' src={book.image}/>
+                    <div>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat in delectus maiores quibusdam nesciunt tempore molestias sint eligendi ea nemo necessitatibus, esse harum atque eos nostrum, maxime assumenda amet a.</p>
+                        <h1 className="text-5xl font-bold">{book.bookName}</h1>
+                        <button className="btn btn-primary mt-8">View The List</button>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     );
 };
