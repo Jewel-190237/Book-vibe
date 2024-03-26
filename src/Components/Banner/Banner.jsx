82d378fa638b95be4c739678b7b086b4de581983
Jewel-190237 <jewel190237@gmail.com>
2024-03-26 19:20:19 +0600

    listed book updated

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,14 +1,18 @@
 
+import { Link } from 'react-router-dom';
 import image from '../images/book1.png';
 const Banner = () => {
     return (
         <div>
             <div className="hero min-h-screen bg-[#1313130D]">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img className='w-1/2 ' src={image}/>
+                    <img className='w-1/2 ' src={image} />
                     <div>
                         <h1 className="text-5xl font-bold">Books to freshen up <br /> your bookshelf</h1>
-                        <button className="btn btn-primary mt-8">View The List</button>
+                        <Link to={'/listedBooks'}>
+                            <button className="btn btn-primary mt-8">View The List</button>
+                        </Link>
+
                     </div>
                 </div>
             </div>
