544273b473597a4366d15e0aac2952410ac41269
Jewel-190237 <190237@ku.ac.bd>
2024-03-29 00:14:01 +0600

    all file added with updated

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 
 
 const Root = () => {
     return (
-        <div className="max-w-7xl mx-auto">
+        <><div className="max-w-7xl mx-auto">
             <Header></Header>
             <Outlet></Outlet>
-        </div>
+        </div><Footer></Footer></>
     );
 };
 
